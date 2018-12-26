969bbae90cb974e5e5d289b4b8280938a0810e22
Owais Alam <owaisstar@gmail.com>
2018-12-26 12:28:33 +0500

    adding click event to all  buttons

--- app/src/main/java/com/itretina/tictactoe/MainActivity.kt
+++ app/src/main/java/com/itretina/tictactoe/MainActivity.kt
@@ -2,11 +2,16 @@ package com.itretina.tictactoe
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClicked(view:View){
+
     }
 }
