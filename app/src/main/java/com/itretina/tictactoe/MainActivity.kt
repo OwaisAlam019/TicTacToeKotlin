461e575e812c74280a4df18f0d8401059b1dc65b
Owais Alam <owaisstar@gmail.com>
2018-12-26 13:49:32 +0500

    check winner after each gameplay

--- app/src/main/java/com/itretina/tictactoe/MainActivity.kt
+++ app/src/main/java/com/itretina/tictactoe/MainActivity.kt
@@ -54,6 +54,68 @@ class MainActivity : AppCompatActivity() {
             activePlayer = 1
         }
         selectedButton.isEnabled = false
+        checkWinner()
     }
-    
+
+    fun checkWinner(){
+        var winner = -1
+
+        //For player 1
+        //row 1
+        if(player1.contains(1) && player1.contains(2) && player1.contains(3)){
+            winner = 1
+        }
+        //row 2
+        if(player1.contains(4) && player1.contains(5) && player1.contains(6)){
+            winner = 1
+        }
+        //row 3
+        if(player1.contains(7) && player1.contains(8) && player1.contains(9)){
+            winner = 1
+        }
+
+        //col 1
+        if(player1.contains(1) && player1.contains(4) && player1.contains(7)){
+            winner = 1
+        }
+        //col 2
+        if(player1.contains(2) && player1.contains(5) && player1.contains(8)){
+            winner = 1
+        }
+        //col 3
+        if(player1.contains(3) && player1.contains(6) && player1.contains(9)){
+            winner = 1
+        }
+
+        //For player 2
+        //row 1
+        if(player2.contains(1) && player2.contains(2) && player2.contains(3)){
+            winner = 2
+        }
+        //row 2
+        if(player2.contains(4) && player2.contains(5) && player2.contains(6)){
+            winner = 2
+        }
+        //row 3
+        if(player2.contains(7) && player2.contains(8) && player2.contains(9)){
+            winner = 2
+        }
+
+        //col 1
+        if(player2.contains(1) && player2.contains(4) && player2.contains(7)){
+            winner = 2
+        }
+        //col 2
+        if(player2.contains(2) && player2.contains(5) && player2.contains(8)){
+            winner = 2
+        }
+        //col 3
+        if(player2.contains(3) && player2.contains(6) && player2.contains(9)){
+            winner = 2
+        }
+
+        if(winner != -1)
+        Toast.makeText(this,"The winner is player"+winner,Toast.LENGTH_SHORT).show()
+
+    }
 }
