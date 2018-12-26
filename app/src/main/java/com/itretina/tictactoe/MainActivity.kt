d1c3005693d7221195bed96db8e9ae5efa9f6fd9
Owais Alam <owaisstar@gmail.com>
2018-12-26 13:20:12 +0500

    assigning cell id to each button

--- app/src/main/java/com/itretina/tictactoe/MainActivity.kt
+++ app/src/main/java/com/itretina/tictactoe/MainActivity.kt
@@ -3,6 +3,7 @@ package com.itretina.tictactoe
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     protected fun buttonClicked(view:View){
-        Toast.makeText(this,"Button clicked..",Toast.LENGTH_LONG).show()
+        val selectedButton = view as Button
+        var cellId = 0
+
+        when(selectedButton.id){
+            R.id.button1 -> cellId = 1
+            R.id.button2 -> cellId = 2
+            R.id.button3 -> cellId = 3
+            R.id.button4 -> cellId = 4
+            R.id.button5 -> cellId = 5
+            R.id.button6 -> cellId = 6
+            R.id.button7 -> cellId = 7
+            R.id.button8 -> cellId = 8
+            R.id.button9 -> cellId = 9
+
+        }
+        Toast.makeText(this,"Button clicked.."+cellId,Toast.LENGTH_SHORT).show()
     }
+    
 }
